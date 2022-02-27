1d7b5a0d24ca442c12dae9eda0d6540e0179dc76
Nichole Mattera <me@nicholemattera.gay>
2022-02-27 14:37:13 -0500

    ✨ Added Intersex Progress Pride and Progress Pride flags

--- src/image-preview/composables/useRendering.js
+++ src/image-preview/composables/useRendering.js
@@ -2,15 +2,26 @@ export default function useRendering(context) {
   return (settings) => {
     if (!context.value) return
 
-    const size = Math.sqrt(
-      Math.pow(settings.width, 2) + Math.pow(settings.height, 2)
-    )
-    const flagHeight =
-      size / settings.flags.filter((flag) => flag.id !== '').length
+    // Used to help build flags. Should always be set to false.
+    const flagBuilderMode = false
 
+    let size, flagHeight
     const tempCanvas = document.createElement('canvas')
-    tempCanvas.height = size
-    tempCanvas.width = size
+    if (flagBuilderMode) {
+      size = settings.width
+      flagHeight = settings.height
+
+      tempCanvas.height = settings.height
+      tempCanvas.width = settings.width
+    } else {
+      size = Math.sqrt(
+        Math.pow(settings.width, 2) + Math.pow(settings.height, 2)
+      )
+      flagHeight = size / settings.flags.filter((flag) => flag.id !== '').length
+
+      tempCanvas.height = size
+      tempCanvas.width = size
+    }
 
     const tempContext = tempCanvas.getContext('2d')
 
@@ -73,7 +84,6 @@ export default function useRendering(context) {
 
         tempContext.save()
         tempContext.strokeStyle = flag.bars[1]
-        tempContext.fillStyle = flag.bars[2]
         tempContext.lineWidth = flagHeight * 0.1
         tempContext.beginPath()
         tempContext.arc(
@@ -84,9 +94,77 @@ export default function useRendering(context) {
           2 * Math.PI
         )
         tempContext.closePath()
-        tempContext.fill()
         tempContext.stroke()
         tempContext.restore()
+      } else if (
+        flag.id === 'intersex-progress-pride' ||
+        flag.id === 'progress-pride'
+      ) {
+        const barHeight = flagHeight / 6
+
+        for (let barIndex = 0; barIndex < 6; barIndex++) {
+          tempContext.fillStyle = flag.bars[barIndex]
+          tempContext.fillRect(
+            0,
+            flagHeight * flagIndex + barHeight * barIndex,
+            size,
+            barHeight
+          )
+        }
+
+        const numberOfTriangles = flag.id === 'intersex-progress-pride' ? 6 : 5
+        const triangleSize = flagHeight * 0.05
+        let triangleXPositionIndex =
+          flag.id === 'intersex-progress-pride' ? 3.5 : 1
+        for (
+          let triangleIndex = 0;
+          triangleIndex < numberOfTriangles;
+          triangleIndex++
+        ) {
+          tempContext.fillStyle = flag.bars[6 + triangleIndex]
+          tempContext.beginPath()
+          tempContext.moveTo(
+            triangleSize * triangleXPositionIndex,
+            flagHeight * flagIndex
+          )
+          tempContext.lineTo(
+            triangleSize * triangleXPositionIndex + triangleSize,
+            flagHeight * flagIndex
+          )
+          tempContext.lineTo(
+            triangleSize * (triangleXPositionIndex + 1) + flagHeight * 0.5,
+            flagHeight * flagIndex + barHeight * 3
+          )
+          tempContext.lineTo(
+            triangleSize * (triangleXPositionIndex + 1),
+            flagHeight * (flagIndex + 1)
+          )
+          tempContext.lineTo(
+            triangleSize * triangleXPositionIndex,
+            flagHeight * (flagIndex + 1)
+          )
+          tempContext.fill()
+
+          triangleXPositionIndex--
+        }
+
+        if (flag.id === 'intersex-progress-pride') {
+          tempContext.save()
+          tempContext.strokeStyle = flag.bars[12]
+          tempContext.lineWidth = flagHeight * 0.03
+          tempContext.beginPath()
+          tempContext.arc(
+            (triangleSize * (triangleXPositionIndex + 1) + flagHeight * 0.5) /
+              2,
+            flagHeight * flagIndex + flagHeight / 2,
+            flagHeight * 0.09375,
+            0,
+            2 * Math.PI
+          )
+          tempContext.closePath()
+          tempContext.stroke()
+          tempContext.restore()
+        }
       } else {
         const barHeight = flagHeight / flag.bars.length
         flag.bars.forEach((bar, barIndex) => {
@@ -104,9 +182,11 @@ export default function useRendering(context) {
     context.value.clearRect(0, 0, settings.width, settings.height)
 
     context.value.save()
-    context.value.translate(settings.width / 2, settings.height / 2)
-    context.value.rotate(-Math.PI / 4)
-    context.value.translate(-size / 2, -size / 2)
+    if (!flagBuilderMode) {
+      context.value.translate(settings.width / 2, settings.height / 2)
+      context.value.rotate(-Math.PI / 4)
+      context.value.translate(-size / 2, -size / 2)
+    }
     context.value.drawImage(tempCanvas, 0, 0)
     context.value.restore()
   }
